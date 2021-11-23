d04abd6b9088265450e772185f0ab78ed43b401f
daraphista <daraphistadaone@gmail.com>
2021-11-23 17:38:26 +0800

    improved code

--- script.js
+++ script.js
@@ -3,28 +3,32 @@ const buttons = Array.from(document.querySelectorAll("button"));
 const number = document.querySelector(".numbers");
 
 buttons.forEach(button => button.addEventListener('click', () => {
-    if(screen.textContent.length < 11) {
-      screen.textContent += button.textContent;
-      if(screen.textContent.includes("=")) {
-        screen.textContent = calculate(screen.textContent);
-      }
+  if(screen.textContent.length < 11) {
+    screen.textContent += button.textContent;
+    if(screen.textContent.includes("=")) {
+      screen.textContent = calculate(screen.textContent);
+      console.log(screen.textContent);
     }
+  }
 }))
 
 function calculate(str) {
+  let result;
+
   str = str.replace(/=/g, "");
-  const arr = str.split(/\D/g);
-  console.log(arr);
+  const arr = str.split(/\+|−|=|÷|×/g);
 
   if(str.includes("+")) {
-    return add(arr[0], arr[1]).toString();
-  } else if(str.includes("−")) {
-    return subtract(arr[0], arr[1].toString());
+    result = add(arr[0], arr[1]);
+    } else if(str.includes("−")) {
+    result = subtract(arr[0], arr[1]);
   } else if(str.includes("×")) {
-    return multiply(arr[0], arr[1].toString());
+    result = multiply(arr[0], arr[1]);
   } else if(str.includes("÷")) {
-    return divide(arr[0], arr[1].toString());
+    result = divide(arr[0], arr[1]);
   }
+
+  return (Number.isInteger(result)) ? result : result.toFixed(2); 
 }
 
 function add(a, b) {
