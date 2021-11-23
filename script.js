d21c63bdc277efa37de271d490861322b919128d
daraphista <daraphistadaone@gmail.com>
2021-11-23 18:59:36 +0800

    fixed decimal bugs and cleaned code

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const screen = document.querySelector(".screen");
 const buttons = Array.from(document.querySelectorAll("button"));
-const number = document.querySelector(".numbers");
+const number = document.querySelector(".number");
+const equals = document.querySelector("#equals");
 
-buttons.forEach(button => button.addEventListener('click', () => {
-  if(screen.textContent.length < 11) {
-    screen.textContent += button.textContent;
-    if(screen.textContent.includes("=")) {
-      screen.textContent = calculate(screen.textContent);
-      console.log(screen.textContent);
-    }
-  }
+buttons.forEach(button => button.addEventListener("click", () => {
+  screen.textContent += button.textContent;
+  screen.textContent = format(screen.textContent);
+  equals.addEventListener("click", () => {
+    screen.textContent = calculate(screen.textContent);
+    console.log(screen.textContent);
+  });
 }))
+
+function format(str) {
+  str = str.replace(/\++/g, "+").replace(/−+/g, "−").replace(/÷+/g, "÷").replace(/×+/g, "×")
+  return (str.match(/[0-9]/)) ? str : str.replace(/\+|−|÷|×|=/g, "");
+}
 
 function calculate(str) {
   let result;
 
-  str = str.replace(/=/g, "");
-  const arr = str.split(/\+|−|=|÷|×/g);
+  str = str.replace(/=/g, ""); 
+  const arr = str.split(/\+|−|÷|×/g);
 
   if(str.includes("+")) {
     result = add(arr[0], arr[1]);
