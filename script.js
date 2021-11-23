729746651b7a46457f0789ecb09b0159b904f198
daraphista <daraphistadaone@gmail.com>
2021-11-23 15:14:09 +0800

    added basic arithmetic functionality

--- script.js
+++ script.js
@@ -6,18 +6,39 @@ buttons.forEach(button => button.addEventListener('click', () => {
     if(screen.textContent.length < 11) {
       screen.textContent += button.textContent;
       if(screen.textContent.includes("=")) {
-        screen.textContent = convert(screen.textContent);
+        screen.textContent = calculate(screen.textContent);
       }
     }
-    console.log(screen.textContent);
 }))
 
-function convert(str) {
-  str = str.replace(/=/g, " ");
-  const arr = str.split("+");
-  return add(arr[0], arr[1]).toString();
+function calculate(str) {
+  str = str.replace(/=/g, "");
+  const arr = str.split(/\D/g);
+  console.log(arr);
+
+  if(str.includes("+")) {
+    return add(arr[0], arr[1]).toString();
+  } else if(str.includes("−")) {
+    return subtract(arr[0], arr[1].toString());
+  } else if(str.includes("×")) {
+    return multiply(arr[0], arr[1].toString());
+  } else if(str.includes("÷")) {
+    return divide(arr[0], arr[1].toString());
+  }
 }
 
 function add(a, b) {
   return +a + +b;
+}
+
+function subtract(a, b) {
+  return +a - +b;
+}
+
+function multiply(a, b) {
+  return +a * +b;
+}
+
+function divide(a, b) {
+  return +a / +b;
 }
