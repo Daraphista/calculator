cac178854064db127599272919bf0f844a027830
daraphista <daraphistadaone@gmail.com>
2021-11-25 14:56:25 +0800

    added clear and negate functionality

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 const screen = document.querySelector(".screen");
 const buttons = Array.from(document.querySelectorAll("button"));
+const displayables = Array.from(document.querySelectorAll(".displayable"))
 const number = document.querySelector(".number");
 const equals = document.querySelector("#equals");
+const backspace = document.querySelector("#backspace");
+const clear = document.querySelector("#clear");
+const negate = document.querySelector("#negate");
 
-buttons.forEach(button => button.addEventListener("click", () => {
+
+displayables.forEach(button => button.addEventListener("click", () => {
+  if(screen.textContent === '0') {
+    screen.textContent = screen.textContent.substring(1);
+  }
   screen.textContent += button.textContent;
-  console.log(screen.textContent);
   screen.textContent = format(screen.textContent);
-  equals.addEventListener("click", () => {
-    screen.textContent = calculate(screen.textContent);
-  });
 }))
+
+equals.addEventListener("click", () => {
+  screen.textContent = calculate(screen.textContent);
+});
+
+backspace.addEventListener("click", () => {
+  screen.textContent = screen.textContent.slice(0, -1);
+  if(screen.textContent === '') {
+    screen.textContent = '0';
+  }
+})
+
+clear.addEventListener("click", () => {
+  screen.textContent = '0';
+})
+
+negate.addEventListener("click", () => {
+  screen.textContent = screen.textContent.insert(screen.textContent.search(/\+|−|÷|×/)+1, '-')
+})
+
+String.prototype.insert = function(index, string) {
+  if (index > 0) {
+    return this.substring(0, index) + string + this.substr(index);
+  }
+
+  return string + this;
+}
 
 function format(str) {
   str = str.replace(/\++/g, "+").replace(/−+/g, "−").replace(/÷+/g, "÷").replace(/×+/g, "×")
-  str = (str.match(/[0-9]/)) ? str : str.replace(/\+|−|÷|×|=/g, "");
+  str = (str.match(/[0-9]/)) ? str : str.replace(/\+|−|÷|×/g, "");
   str = (str.match(/[0-9]\D\D/)) ? str.slice(0, -1) : str;
+  str = str.replace(/\./g, (val, index, str) => index === str.indexOf('.') ? val : '');
   return str;
 }
 
